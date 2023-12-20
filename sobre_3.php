ae94791df84f9de1f451285714595d75f8aa3658
anajfalfa <anajfalfaiate@gmail.com>
2023-12-20 22:24:40 +0000

    index e sobre

---
+++ sobre_3.php
@@ -0,0 +1,152 @@
+<<<<<<< HEAD
+<head>
+    <link rel="stylesheet" href="assets/styles/style_geral.css">
+    <link rel="stylesheet" href="assets/styles/style_sobre.css">
+</head>
+
+
+<body>
+    <div class="fundo-roxo">
+        <h1 class="titulo-branco"  style="text-align:center">TEMAS DESTE ANO</h1>
+        
+        <div class="temas">
+            <div> <a href="">
+                <div class="content"> 
+                    <h4>Mobilidade Assistida </h4>
+                    <p>Da inovação à acessibilidade</p> 
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4> R(e)volucionando a engenharia de tecidos</h2>
+                    <p>Do cultivo à impressão</p>
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4>A Nova Saúde</h4>
+                    <p>Wearables & Telemonitorização</p>
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4> Inteligência Artificial</h4>
+                    <p>Na Saúde</p>
+                </div>
+            </a>
+            </div>
+        </div>
+    
+
+
+
+
+    </div>
+    
+=======
+<head>
+    <link rel="stylesheet" href="assets/styles/style_geral.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Oswald">
+</head>
+
+<style>
+    
+    /**  .temas{
+        width:100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        padding: 6rem 2rem;
+        gap: 10px;
+    } */
+
+    .temas{
+        display: grid;
+        grid-template-columns: 23% 23% 23% 23%;
+        justify-content: center;
+        gap: 1.5%;
+
+        > div {
+            /**background: #3F165B url('../img/testemunhos/titulo.png') ;
+            background-image: linear-gradient(to top, #3F165B, #0f2d4c00), url(../img/background_inicio_homepage.png);*/
+            background-image: linear-gradient(to top, #3F165B, #0f2d4c00), url(./assets/img/job2be_signup.png);
+            opacity: 90%;
+            height: 250px;
+            padding: 5px;
+            margin-top: 20px;
+            box-sizing: border-box;
+            border-radius: 20px;
+            border: 2px solid #3F165B;
+            display: grid;
+            align-items: flex-end;
+            padding-bottom: 15px;
+
+            a {
+                text-decoration: none;
+            }
+
+            h4{
+                font-family: Oswald;
+                text-align: center;
+                font-weight: bold;
+                color: #fff;
+            }
+
+            p{
+                font-family: Oswald;
+                text-align: center;
+                font-weight: normal;
+                color: #fff;
+            }
+        }
+    }
+
+
+</style>
+
+<body>
+    <div class="fundo-roxo">
+        <h1 class="titulo-branco"  style="text-align:center">TEMAS DESTE ANO</h1>
+        
+        <div class="temas">
+            <div> <a href="">
+                <div class="content"> 
+                    <h4>Mobilidade Assistida </h4>
+                    <p>Da inovação à acessibilidade</p> 
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4> R(e)volucionando a engenharia de tecidos</h2>
+                    <p>Do cultivo à impressão</p>
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4>A Nova Saúde</h4>
+                    <p>Wearables & Telemonitorização</p>
+                </div>
+            </a>
+            </div>
+            <div> <a href="">
+                <div class="content"> 
+                    <h4> Inteligência Artificial</h4>
+                    <p>na Saúde</p>
+                </div>
+            </a>
+            </div>
+        </div>
+    
+
+
+
+
+    </div>
+    
+>>>>>>> refs/remotes/origin/main
+</body>
