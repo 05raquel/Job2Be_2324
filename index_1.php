9281685009f1f86e83f0d58afd3af276029b5d85
05raquel <raquel.neves.coelho@tecnico.ulisboa.pt>
2024-01-23 18:15:08 +0000

    counter index-1

--- index_1.php
+++ index_1.php
@@ -1,3 +1,34 @@
 <head>
     <link rel="stylesheet" href="assets/styles/style_geral.css">
+    <link rel="stylesheet" href="assets/styles/style_index_1.css">
 </head>
+<body>
+    <div class="line">
+        <div class="image">
+            <img src="assets/img/savethedateimg.png">
+        </div>
+        <div class="counter">
+            <div class="until" id="myCountdown">
+                <div class="until__component">
+                    <div class="until__numeric until__numeric--days">00</div>
+                    <div class="until__unit">DIAS</div>
+                </div>
+                <div class="until__component">
+                    <div class="until__numeric until__numeric--hours">00</div>
+                    <div class="until__unit">HORAS</div>
+                </div>
+                <div class="until__component">
+                    <div class="until__numeric until__numeric--minutes">00</div>
+                    <div class="until__unit">MIN</div>
+                </div>
+                <div class="until__component">
+                    <div class="until__numeric until__numeric--seconds">00</div>
+                    <div class="until__unit">SEG</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.10/dayjs.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.10/plugin/duration.min.js"></script>
+    <script src="assets/javascript/counter.js"></script>
+</body>
