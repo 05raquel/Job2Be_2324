252efb391a14ed46ab7bd9b4901f6ee0902ddc08
anajfalfa <anajfalfaiate@gmail.com>
2024-01-24 10:01:40 +0000

    acertar commits e colocar imagens no index3

--- index_3.php
+++ index_3.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <head>
     <link rel="stylesheet" href="assets/styles/style_geral.css">
     <link rel="stylesheet" href="assets/styles/style_index.css">
@@ -8,31 +7,31 @@
     <div class="fundo-roxo">
         <h1 class="titulo-branco" style="text-align:right">ATIVIDADES</h1>
         <div class="atividades">
-            <div> <a href="">
+            <div class="palestra"> <a href="">
                 <div class="content"> 
                     <h4>PALESTRAS</h4>
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="workshop"> <a href="">
                 <div class="content"> 
                     <h4>WORKSHOPS</h2>
                 </div>
             </a>
-            </div>
-            <div> <a href="">
+            </div> 
+            <div class="pitch"> <a href="">
                 <div class="content"> 
                     <h4>PITCH</h4>
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="feira_e"> <a href="">
                 <div class="content"> 
                     <h4>FEIRA DE EMPRESAS</h4>
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="alumni"> <a href="">
                 <div class="content"> 
                     <h4>ALUMNI SESSION</h4>
                 </div>
@@ -42,96 +41,4 @@
 
     </div>
     
-=======
-<head>
-    <link rel="stylesheet" href="assets/styles/style_geral.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Oswald">
-</head>
-
-
-<style>
-
-    .atividades{
-        display: grid;
-        grid-template-columns: 18% 18% 18% 18% 18%;
-        justify-content: center;
-        gap: 1.5%;
-
-        > div {
-            /**background: #3F165B url('../img/testemunhos/titulo.png') ;
-            background-image: linear-gradient(to top, #3F165B, #0f2d4c00), url(../img/background_inicio_homepage.png);*/
-            background-image: linear-gradient(to top, #3F165B, #0f2d4c00), url(./assets/img/job2be_signup.png);
-            opacity: 90%;
-            height: 250px;
-            padding: 5px;
-            margin-top: 20px;
-            box-sizing: border-box;
-            border-radius: 20px;
-            border: 2px solid #3F165B;
-            display: grid;
-            align-items: flex-end;
-            padding-bottom: 15px;
-
-            a {
-                text-decoration: none;
-            }
-
-            h4{
-                font-family: Oswald;
-                text-align: center;
-                font-weight: bold;
-                color: #fff;
-            }
-
-            p{
-                font-family: Oswald;
-                text-align: center;
-                font-weight: normal;
-                color: #fff;
-            }
-        }
-    }
-
-
-</style>
-
-<body>
-    <div class="fundo-roxo">
-        <h1 class="titulo-branco" style="text-align:right">ATIVIDADES</h1>
-        <div class="atividades">
-            <div> <a href="">
-                <div class="content"> 
-                    <h4>PALESTRAS</h4>
-                </div>
-            </a>
-            </div>
-            <div> <a href="">
-                <div class="content"> 
-                    <h4>WORKSHOPS</h2>
-                </div>
-            </a>
-            </div>
-            <div> <a href="">
-                <div class="content"> 
-                    <h4>PITCH</h4>
-                </div>
-            </a>
-            </div>
-            <div> <a href="">
-                <div class="content"> 
-                    <h4>FEIRA DE EMPRESAS</h4>
-                </div>
-            </a>
-            </div>
-            <div> <a href="">
-                <div class="content"> 
-                    <h4>ALUMNI SESSION</h4>
-                </div>
-            </a>
-            </div>
-        </div>
-
-    </div>
-    
->>>>>>> refs/remotes/origin/main
 </body>
