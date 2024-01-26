ae30b2c11ec502c9d2aa476f1d992bbd780a7a38
anajfalfa <anajfalfaiate@gmail.com>
2024-01-26 21:10:37 +0000

    temas do ano

--- sobre_3.php
+++ sobre_3.php
@@ -9,28 +9,28 @@
         <h1 class="titulo-branco"  style="text-align:center">TEMAS DESTE ANO</h1>
         
         <div class="temas">
-            <div> <a href="">
+            <div class="mobilidade"> <a href="">
                 <div class="content"> 
                     <h4>Mobilidade Assistida </h4>
                     <p>Da inovação à acessibilidade</p> 
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="eng_tecidos"> <a href="">
                 <div class="content"> 
                     <h4> R(e)volucionando a engenharia de tecidos</h2>
                     <p>Do cultivo à impressão</p>
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="nova_era_saude"> <a href="">
                 <div class="content"> 
-                    <h4>A Nova Saúde</h4>
-                    <p>Wearables & Telemonitorização</p>
+                    <h4>A Nova Era na Saúde</h4>
+                    <p>Telemonitorização & Telemedicina</p>
                 </div>
             </a>
             </div>
-            <div> <a href="">
+            <div class="ia_saude"> <a href="">
                 <div class="content"> 
                     <h4> Inteligência Artificial</h4>
                     <p>Na Saúde</p>
