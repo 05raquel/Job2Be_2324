e8f11ae9cdf2f8120599dbb5e4fbe3e7a696bd09
anajfalfa <anajfalfaiate@gmail.com>
2024-02-03 19:49:40 +0000

    atualizacao

--- index_2.php
+++ index_2.php
@@ -3,13 +3,12 @@
     <link rel="stylesheet" href="assets/styles/style_menu.css">
 </head>
 <body>
-    <?php include("includes/new_menu.php");?>
     <div class="fundo-branco">
         <table style="width: 100%;">
             <tr>
                 <td style="text-align: left;">
-                    <div class="imagem">
-                        <img src="assets/img/o_que_e_branco.png">
+                    <div class="imagem-o-que-e">
+                        <img src="assets/img/o_que_e_roxo.png">
                     </div>
                     <div class="item">
                         <a href="sobre.php">
@@ -24,6 +23,16 @@
                 </td>
             </tr>
         </table>
+        <div class="imagem-o-que-e">
+            <img src="assets/img/o_que_e_roxo.png">
+        </div>
+        <div class="item">
+            <a href="sobre.php">
+                <button class="menu-button" style="background-color: #3F165B; color: #FFFFFF; border-radius: 15px;">saber +</button>
+            </a>
+        </div>
+        <div class="texto-roxo">
+            A JOB2BE - Semana da Biomédica 2024 é um evento organizado pelo Núcleo de Estudantes de Engenharia Biomédica do Instituto Superior Técnico da Universidade de Lisboa (NEBM-IST) que visa estreitar a relação entre a comunidade académica, científica e o mundo empresarial, assim como divulgar o trabalho desenvolvido atualmente na área da Engenharia Biomédica.
+        </div>
     </div>
-    <?php include("includes/new_footer.php");?>
 </body>
